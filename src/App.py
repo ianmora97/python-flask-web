98bf8630b2687c0ada6196a65e5db8f917e1348b
ianmora97 <ianmorar03@gmail.com>
2024-05-15 10:30:17 -0600

    gitignore

--- src/App.py
+++ src/App.py
@@ -1,8 +1,6 @@
-# Estos son los imports que se ocupan de momento para poder correr el servidor
 from flask import Flask;
 from Routes import app;
 
 # * Servidor
 if __name__ == '__main__':
-    # Se le crea la configuración al servidor
     app.run(host='localhost', port=3000, debug=True,use_reloader=True);
